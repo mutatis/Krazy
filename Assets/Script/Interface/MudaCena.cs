8d0543e3031318d7ac1a3bce5875317cba8c3edd
Gabriel <mutatisguga@gmail.com>
2015-05-28 12:29:09 -0300

    Melhorias

---
+++ Assets/Script/Interface/MudaCena.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MudaCena : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Application.LoadLevel("Game");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Script/Lista.cs
+++ Assets/Script/Lista.cs
@@ -18,23 +18,24 @@ public class Lista : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
+		print (obj.Count + " " + obj2.Count);
 		if(x == 4)
 		{
-			if(obj.Count > obj2.Count && obj.Count >= 4)
+			if(obj.Count > obj2.Count && obj.Count > 3)
 			{
 				for(int i = 0; i < obj.Count; i++)
 				{
 					Destroy ((GameObject)obj[i]);
 				}
 			}
-			else if(obj.Count < obj2.Count && obj2.Count >= 4)
+			else if(obj.Count < obj2.Count && obj2.Count > 3)
 			{
 				for(int i = 0; i < obj2.Count; i++)
 				{
 					Destroy ((GameObject)obj2[i]);
 				}
 			}
-			else 
+			else
 			{
 				Destroy(gameObject);
 			}
