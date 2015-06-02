9e106033e5ef90aede69a4aadb28a9acf7300f3f
Gabriel <mutatisguga@gmail.com>
2015-06-02 11:18:28 -0300

    organizacao

---
+++ Assets/Script/CorCol.cs
@@ -0,0 +1,169 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CorCol : MonoBehaviour 
+{
+	public List<string> tagsBlock;
+	public SpriteRenderer sprite;
+    bool selectColorOK = true;
+    Color cor;
+
+    // Use this for initialization
+    void Start()
+    {
+        cor = new Color(sprite.color.r, sprite.color.g, sprite.color.b, sprite.color.a);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Input.GetMouseButton(0) && selectColorOK)
+        {
+            sprite.color = cor;
+            selectColorOK = false;
+        }
+    }
+	public bool ok;
+
+	// Use this for initialization
+	
+	// Update is called once per frame
+
+	public void OnSelect()
+	{
+        print("selected");
+        sprite.color = Color.white;
+        selectColorOK = true;
+	}
+
+    public void OnRemove() 
+    {
+        sprite.color = cor;
+        selectColorOK = true;
+    }
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(tagsBlock.Any(tag => tag == collision.gameObject.tag))
+		{
+			ok = false;
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D collision)
+	{
+		if(tagsBlock.Any(tag => tag == collision.gameObject.tag))
+		{
+			ok = false;
+		}
+	}
+	
+	void OnCollisionExit2D(Collision2D collision)
+	{
+		if(tagsBlock.Any(tag => tag == collision.gameObject.tag))
+		{
+			ok = true;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(tagsBlock.Any(tag => tag == collision.gameObject.tag))
+		{
+			ok = false;
+		}
+	}
+	
+	void OnTriggerStay2D(Collider2D collision)
+	{
+		if(tagsBlock.Any(tag => tag == collision.gameObject.tag))
+		{
+			ok = false;
+		}
+	}
+	
+	void OnTriggerExit2D(Collider2D collision)
+	{
+		if(tagsBlock.Any(tag => tag == collision.gameObject.tag))
+		{
+			ok = true;
+		}
+	}
+}
+
+/*using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CorCol : MonoBehaviour 
+{
+	public List<string> tagsBlock;
+	Color cor;
+	public SpriteRenderer sprite;
+    bool selectColorOK = true;
+	
+
+	// Use this for initialization
+	void Start ()
+	{
+		cor = new Color(sprite.color.r, sprite.color.g, sprite.color.b, sprite.color.a);	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        if (!Input.GetMouseButton(0) && selectColorOK)
+        {
+            sprite.color = cor;
+            selectColorOK = false;
+        }
+	}
+
+	public void OnSelect()
+	{
+        sprite.color = Color.white;
+        selectColorOK = true;
+	}
+
+	public void OnRemove()
+	{
+        sprite.color = cor;
+        selectColorOK = true;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(tagsBlock.Any(tag => tag == collision.gameObject.tag))
+		{
+			sprite.color = Color.white;
+		}
+	}
+	
+	void OnCollisionExit2D(Collision2D collision)
+	{
+		if(tagsBlock.Any(tag => tag == collision.gameObject.tag))
+		{
+			sprite.color = new Color(cor.r, cor.g, cor.b, cor.a);
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(tagsBlock.Any(tag => tag == collision.gameObject.tag))
+		{
+			sprite.color = Color.white;
+		}
+	}
+	
+	void OnTriggerExit2D(Collider2D collision)
+	{
+		if(tagsBlock.Any(tag => tag == collision.gameObject.tag))
+		{
+            sprite.color = new Color(cor.r, cor.g, cor.b, cor.a);
+		}
+	}
+}
+*/
---
+++ Assets/Script/Creat/CreatedObj.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CreatedObj : MonoBehaviour 
+{
+
+	public GameObject[] grid;
+	public GameObject[] created;
+
+	public Transform canvas;
+
+	int pode;
+
+	int gridRandom;
+	int createdRandom;
+
+	// Use this for initialization
+	void Start () 
+	{
+		grid = GameObject.FindGameObjectsWithTag("Grid");
+		StartCoroutine("GO");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	IEnumerator GO()
+	{
+		pode = 0;
+		yield return new WaitForSeconds (2);
+		gridRandom = Random.Range (0, grid.Length);
+		while(pode < 0)
+		{
+			if(grid[gridRandom].GetComponent<CorCol>().ok == false)
+			{
+				gridRandom = Random.Range (0, grid.Length);
+				pode = 0;
+			}
+			else if(grid[gridRandom].GetComponent<CorCol>().ok == true)
+			{
+				pode = 1;
+			}
+		}
+		createdRandom = Random.Range (0, created.Length);
+		GameObject obj = Instantiate (created [createdRandom], grid [gridRandom].transform.position, transform.rotation) as GameObject;
+		obj.transform.localScale = new Vector3 (0.01f, 0.01f, 0.01f);
+		obj.transform.parent = this.gameObject.transform;
+		pode = 0;
+		StartCoroutine("GO");
+	}
+}
