9d1f63a8d4a3f5b188c46f6fa285faf8cf54e7cd
Gabriel <mutatisguga@gmail.com>
2015-06-03 13:07:53 -0300

    d

--- Assets/Script/BlockSquare.cs
+++ Assets/Script/BlockSquare.cs
@@ -21,6 +21,9 @@ public class BlockSquare : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+		if (blockStack < 0)
+			blockStack = 0;
+
         if (!Input.GetMouseButton(0) && selectColorOK)
         {
             sprite.color = cor;
@@ -102,78 +105,3 @@ public class BlockSquare : MonoBehaviour
 		}
 	}
 }
-
-/*using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-
-public class CorCol : MonoBehaviour 
-{
-	public List<string> tagsBlock;
-	Color cor;
-	public SpriteRenderer sprite;
-    bool selectColorOK = true;
-	
-
-	// Use this for initialization
-	void Start ()
-	{
-		cor = new Color(sprite.color.r, sprite.color.g, sprite.color.b, sprite.color.a);	
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-        if (!Input.GetMouseButton(0) && selectColorOK)
-        {
-            sprite.color = cor;
-            selectColorOK = false;
-        }
-	}
-
-	public void OnSelect()
-	{
-        sprite.color = Color.white;
-        selectColorOK = true;
-	}
-
-	public void OnRemove()
-	{
-        sprite.color = cor;
-        selectColorOK = true;
-	}
-
-	void OnCollisionEnter2D(Collision2D collision)
-	{
-		if(tagsBlock.Any(tag => tag == collision.gameObject.tag))
-		{
-			sprite.color = Color.white;
-		}
-	}
-	
-	void OnCollisionExit2D(Collision2D collision)
-	{
-		if(tagsBlock.Any(tag => tag == collision.gameObject.tag))
-		{
-			sprite.color = new Color(cor.r, cor.g, cor.b, cor.a);
-		}
-	}
-	
-	void OnTriggerEnter2D(Collider2D collision)
-	{
-		if(tagsBlock.Any(tag => tag == collision.gameObject.tag))
-		{
-			sprite.color = Color.white;
-		}
-	}
-	
-	void OnTriggerExit2D(Collider2D collision)
-	{
-		if(tagsBlock.Any(tag => tag == collision.gameObject.tag))
-		{
-            sprite.color = new Color(cor.r, cor.g, cor.b, cor.a);
-		}
-	}
-}
-*/
