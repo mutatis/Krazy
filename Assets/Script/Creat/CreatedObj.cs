4ad7b3cc3207e054bbf6581bb6ff016bcf2d7387
Westerbly Snaydley <westerbly@snaydley.com>
2015-06-05 14:21:17 -0300

    Novas features
    Agora os blocos são lançados em waves, e o movimento de chegada é
    senoide.

--- Assets/Script/Creat/CreatedObj.cs
+++ Assets/Script/Creat/CreatedObj.cs
@@ -4,23 +4,19 @@ using UnityEngine.UI;
 
 public class CreatedObj : MonoBehaviour 
 {
-
-	public static CreatedObj creat;
-
 	public GameObject[] grid;
 	public GameObject[] created;
-
 	public Transform canvas;
+    public float tempoWave;
+    public int startingWave;
+    public int minWave;
+    public int maxWave;
+    public int wave = 0;
 
 	int pode = -1;
 
 	public int gridRandom;
 	int createdRandom;
-
-	void Awake()
-	{
-		creat = this;
-	}
 
 	// Use this for initialization
 	void Start () 
@@ -38,24 +34,35 @@ public class CreatedObj : MonoBehaviour
 	IEnumerator GO()
 	{
 		pode = 0;
-		yield return new WaitForSeconds (2);
-		gridRandom = Random.Range (0, grid.Length);
-		do
-		{
-			if(grid[gridRandom].GetComponent<BlockSquare>().ok == false)
-			{
-				gridRandom = Random.Range (0, grid.Length);
-				pode = -1;
-			}
-			else if(grid[gridRandom].GetComponent<BlockSquare>().ok == true)
-			{
-				pode = 1;
-			}
-		}while(pode < 0);
-		createdRandom = Random.Range (0, created.Length);
-		GameObject obj = Instantiate (created [createdRandom], new Vector2(0, -10), transform.rotation) as GameObject;
-		obj.transform.localScale = new Vector3 (0.01f, 0.01f, 0.01f);
-		obj.transform.parent = this.gameObject.transform;
+        yield return new WaitForSeconds(tempoWave);
+        var rdm = new System.Random();
+        var qtdWave = wave++ == 0 ? startingWave : rdm.Next(minWave, maxWave);//range não inclui o maior extremo
+        print(qtdWave);
+        for (int i = 0; i < qtdWave; i++)
+        {
+            gridRandom = rdm.Next(0, grid.Length);
+            var target = grid[gridRandom];
+            do
+            {
+                if (grid[gridRandom].GetComponent<BlockSquare>().ok == false)
+                {
+                    gridRandom = rdm.Next(0, grid.Length);
+                    pode = -1;
+                }
+                else if (grid[gridRandom].GetComponent<BlockSquare>().ok == true)
+                {
+                    target = grid[gridRandom];
+                    pode = 1;
+                }
+                yield return new WaitForEndOfFrame();
+            } while (pode < 0);
+            createdRandom = Random.Range(0, created.Length);
+            GameObject obj = Instantiate(created[createdRandom], new Vector2(0, -10), transform.rotation) as GameObject;
+            obj.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+            obj.transform.parent = this.gameObject.transform;
+            obj.SendMessage("SetTarget", target);
+            obj.GetComponent<SelectPOsition>().StartCoroutine("GoToTarget");
+        }
 		pode = 0;
 		StartCoroutine("GO");
 	}
--- Assets/Script/Itens/SelectPOsition.cs
+++ Assets/Script/Itens/SelectPOsition.cs
@@ -11,8 +11,8 @@ public class SelectPOsition : MonoBehaviour
 	public AudioClip spawn;
 
 	private Vector3 velocity = Vector3.zero;
-
-	float dist;
+    GameObject target;
+    public float noiseRange;
 
 	bool pare;
 
@@ -25,15 +25,15 @@ public class SelectPOsition : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		if(!pare)
+		/*if(!pare)
 		{
-			mouse.quadradoSelecionado = (GameObject)CreatedObj.creat.grid[CreatedObj.creat.gridRandom];
-			dist = Vector3.Distance(CreatedObj.creat.grid[CreatedObj.creat.gridRandom].transform.position, transform.position);
+			mouse.quadradoSelecionado = target;
+			dist = Vector3.Distance(target.transform.position, transform.position);
 			if(dist > 0.1f)
 			{
                 mouse.playingAnimation = true;
-				direction2 = (CreatedObj.creat.grid[CreatedObj.creat.gridRandom].transform.position - transform.position);
-				Vector3 destination = transform.position + direction2;
+				direction2 = (target.transform.position - transform.position);
+                Vector3 destination = transform.position + direction2;
 				transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, 0.25f);
 			}
 			else
@@ -41,10 +41,41 @@ public class SelectPOsition : MonoBehaviour
 				
 				mouse.enabled = true;
 				mouse.pode = true;
-                mouse.quadradoSelecionado = (GameObject)CreatedObj.creat.grid[CreatedObj.creat.gridRandom];
+                mouse.quadradoSelecionado = target;
                 mouse.playingAnimation = false;
 				pare = true;
 			}
-		}
+		}*/
 	}
+
+    IEnumerator GoToTarget()
+    {
+        var rnd = new System.Random();
+        mouse.quadradoSelecionado = target;
+        var dist = Vector3.Distance(target.transform.position, transform.position);
+        var direction2 = (target.transform.position - transform.position);
+        var noiseDirection = new Vector3(-direction2.y, direction2.x, 0).normalized;
+        var magnitudeInicial = direction2.magnitude;
+        while(dist > .1f) 
+        {
+            direction2 = (target.transform.position - transform.position);
+            var noise = (rnd.NextDouble() + Mathf.Sin(Time.time * 15)) * noiseRange;
+            mouse.playingAnimation = true;
+            
+            Vector3 destination = transform.position + direction2;
+            transform.position = Vector3.SmoothDamp(transform.position, destination + (noiseDirection * (float)noise * (direction2.magnitude / magnitudeInicial)), ref velocity, 0.25f);
+            yield return new WaitForEndOfFrame();
+        }
+        print("terminou");
+        mouse.enabled = true;
+        mouse.pode = true;
+        mouse.quadradoSelecionado = target;
+        mouse.playingAnimation = false;
+    } 
+
+    public void SetTarget(GameObject target)
+    {
+        this.target = target;
+        target.GetComponent<BlockSquare>().ok = false;
+    }
 }
