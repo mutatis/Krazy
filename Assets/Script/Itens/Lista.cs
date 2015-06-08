f6e144057d89d84f5762c5deab3ba07e07e76f91
Gabriel <mutatisguga@gmail.com>
2015-06-08 14:38:04 -0300

    L
    Agora os pontos em L T funcionam

--- Assets/Script/Itens/Lista.cs
+++ Assets/Script/Itens/Lista.cs
@@ -30,7 +30,56 @@ public class Lista : MonoBehaviour
 	{
 		if(x == 4)
 		{
-			if(obj.Count > quant)
+			if(obj.Count > quant + 1 && obj2.Count > quant + 1)
+			{
+				int temp;
+				temp = obj.Count + obj2.Count;
+				if(!toca)
+				{
+					switch(temp)
+					{
+					case 3:
+						AudioSource.PlayClipAtPoint(somPontuacao[0], transform.position, 1);
+						break;
+						
+					case 4:
+						AudioSource.PlayClipAtPoint(somPontuacao[1], transform.position, 1);
+						break;
+						
+					case 5:
+						AudioSource.PlayClipAtPoint(somPontuacao[2], transform.position, 1);
+						break;
+						
+					case 6:
+						AudioSource.PlayClipAtPoint(somPontuacao[3], transform.position, 1);
+						break;
+						
+					case 7:
+						AudioSource.PlayClipAtPoint(somPontuacao[4], transform.position, 1);
+						break;
+						
+					case 8:
+						AudioSource.PlayClipAtPoint(somPontuacao[5], transform.position, 1);
+						break;
+						
+					default:
+						AudioSource.PlayClipAtPoint(somPontuacao[6], transform.position, 1);
+						break;
+					}
+					toca = true;
+				}
+				for(int i = 0; i < obj.Count; i++)
+				{
+					objeto = (GameObject)obj[i];
+					if(objeto != null)
+					{
+						anim = objeto.GetComponentInChildren<Animator>();
+						anim.SetTrigger("Kill");
+					}
+					//Destroy ((GameObject)obj[i]);
+				}
+			}
+			else if(obj.Count > quant)
 			{
 				if(!toca)
 				{
@@ -76,9 +125,18 @@ public class Lista : MonoBehaviour
 					}
 					//Destroy ((GameObject)obj[i]);
 				}
+				for(int i = 0; i < obj2.Count; i++)
+				{
+					objeto = (GameObject)obj2[i];
+					if(objeto != null)
+					{
+						anim = objeto.GetComponentInChildren<Animator>();
+						anim.SetTrigger("Kill");
+					}
+					//Destroy ((GameObject)obj2[i]);
+				}
 			}
-
-			if(obj2.Count > quant)
+			else if(obj2.Count > quant)
 			{
 				if(!toca)
 				{
