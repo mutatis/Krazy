2809f55d16b12ee2ec2d76521273b713f4e6e626
Westerbly Snaydley <westerbly@snaydley.com>
2015-06-08 09:43:21 -0300

    Block script

--- Assets/Script/Block.cs
+++ Assets/Script/Block.cs
@@ -3,37 +3,19 @@ using System.Collections;
 
 public class Block : MonoBehaviour
 {
-    public int minimumPopStack;
-	//[HideInInspector]
-    public int x, y;
-    public int type;
-    public BlockInternal blockInternal;
-	Vector2 target;
+    public AudioClip spawnSound;
 
-    // Use this for initialization
+    void Awake()
+    {
+
+    }
     void Start()
     {
-		x = (int)transform.position.x;
-		y = (int)transform.position.y;
-		blockInternal = new BlockInternal(x, y, 0, minimumPopStack, Grid.gridS);
-		Grid.gridS.InsertBlock (blockInternal);
-		blockInternal.blockExternal = this;
+
     }
-
-	void OnMouseUp()
-	{
-		target = new Vector2((int)transform.position.x, (int)transform.position.y);
-		Grid.gridS.MoveBlock (blockInternal, new Vector2 (x, y), target);
-	}
-
-	public void Pop()
-	{
-		Destroy (gameObject);
-	}
-
-    // Update is called once per frame
     void Update()
-    {		
-	
+    {
+
     }
+
 }
