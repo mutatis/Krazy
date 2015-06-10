6f38956145ad103d544bc4534ce7b5a7fda1f89c
Gabriel <mutatisguga@gmail.com>
2015-06-10 10:54:33 -0300

    Inicio Mapa

---
+++ Assets/Script/MapaMov.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapaMov : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate (Input.GetAxis ("Mouse X"), Input.GetAxis ("Mouse Y"), 0);
+	}
+}
