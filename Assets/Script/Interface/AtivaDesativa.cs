09beb8bb956e6f78e42061f12488a4b819e237e5
Gabriel <mutatisguga@gmail.com>
2015-06-11 17:33:54 -0300

    Quinta

---
+++ Assets/Script/Interface/AtivaDesativa.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AtivaDesativa : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void AD(GameObject obj)
+	{
+		if(obj.active)
+		{
+			obj.SetActive(false);
+		}
+		else
+		{
+			obj.SetActive(true);
+		}
+	}
+}
