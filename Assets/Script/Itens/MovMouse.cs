91737bfbec482c6f560fd1de2023c4e52975e384
Westerbly Snaydley <westerbly@snaydley.com>
2015-06-12 16:06:05 -0300

    tentativa de eliminar o Destroy()

--- Assets/Script/Itens/MovMouse.cs
+++ Assets/Script/Itens/MovMouse.cs
@@ -34,15 +34,14 @@ public class MovMouse : MonoBehaviour
         box.enabled = false;
         quadradoSelecionado.SendMessage("OnExit");
         pode = false;
-		quadradoSelecionado.SendMessage ("LockBlock", gameObject);
         print("Killed, stack: " + quadradoSelecionado.GetComponent<BlockSquare>().blockStack.ToString());
+        Score.score.Ponto(1);
+        Destroy(gameObject);
 	}
 
 	public void Destroy()
 	{
-		Score.score.Ponto (1);
-		quadradoSelecionado.SendMessage ("UnlockBlock", gameObject);
-		Destroy(gameObject);
+        
 	}
 
 	public void Down()
