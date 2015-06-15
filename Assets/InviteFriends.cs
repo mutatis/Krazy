2031ee1c9ee0e798146b25e87782a46c3b1303b0
Gabriel <mutatisguga@gmail.com>
2015-06-15 15:33:39 -0300

    Invite friends melhorado

--- Assets/InviteFriends.cs
+++ Assets/InviteFriends.cs
@@ -18,20 +18,42 @@ public class InviteFriends : MonoBehaviour
 
 	public void Invite()
 	{
-		FB.AppRequest(
-			message: "Come play this great game!", 
-			callback: LogCallback
-			);
+		if(FB.IsLoggedIn)
+		{
+			FB.AppRequest(
+				message: "Come play this great game!", 
+				callback: LogCallback
+				);
+		}
+		else
+		{
+			FB.Login("user_about_me, user_relationships, user_birthday, user_location", FBLoginCallback);
+		}
 	}
 	
 	public void Share()
 	{
-		FB.Feed(                                                                                                                 
-		        linkCaption: "Play Game Man",               
-		        picture: "http://static.wixstatic.com/media/b5559f_f5c06b11d0c94058bf2eeaea5b239607.png_srz_p_600_356_75_22_0.50_1.20_0.00_png_srz",                                                     
-		        linkName: "Play linkname",                                                                 
-		        link: "http://apps.facebook.com/" + FB.AppId + "/?challenge_brag=" + (FB.IsLoggedIn ? FB.UserId : "guest")       
-		        ); 
+		if(FB.IsLoggedIn)
+		{
+			FB.Feed(                                                                                                                 
+			        linkCaption: "Play Game Man",               
+			        picture: "http://static.wixstatic.com/media/b5559f_f5c06b11d0c94058bf2eeaea5b239607.png_srz_p_600_356_75_22_0.50_1.20_0.00_png_srz",                                                     
+			        linkName: "Play linkname",                                                                 
+			        link: "http://apps.facebook.com/" + FB.AppId + "/?challenge_brag=" + (FB.IsLoggedIn ? FB.UserId : "guest")       
+			        ); 
+		}
+		else
+		{
+			FB.Login("user_about_me, user_relationships, user_birthday, user_location", FBLoginCallback);
+		}
+	}
+
+	private void FBLoginCallback(FBResult result) {
+		if(FB.IsLoggedIn) {
+			Debug.Log ("Logo");
+		} else {
+			Debug.Log ("FBLoginCallback: User canceled login");
+		}
 	}
 
 	void LogCallback(FBResult response) 
