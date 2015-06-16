0bfb7e508f21ebbdd189849da573cdc2d4f2be81
Gabriel <mutatisguga@gmail.com>
2015-06-16 17:33:47 -0300

    Pause

---
+++ Assets/PauseManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseManager : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		Time.timeScale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		/*if(gameObject.active)
+		{
+			Time.timeScale = 0;
+		}*/
+	}
+
+	public void Resume()
+	{
+		Time.timeScale = 1;
+		gameObject.SetActive (false);
+	}
+
+	public void Menu(string level)
+	{
+		PlayerPrefs.SetString ("Loading", level);
+		Application.LoadLevel ("Loading");
+	}
+}
