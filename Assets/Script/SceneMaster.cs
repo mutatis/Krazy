d64bcc195b31c553883d38343eeb95ef217a8af2
Westerbly Snaydley <westerbly@snaydley.com>
2015-06-17 14:44:25 -0300

    Scene master

---
+++ Assets/Script/SceneMaster.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneMaster : MonoBehaviour {
+    public int metaPontuacao;
+    public int tempoLimiteFase;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void GameOver()
+    {
+
+    }
+}
