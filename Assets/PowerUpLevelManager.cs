c26dc8b4fbac8a712bda68dbf1b86c7b48e8faf0
Gabriel <mutatisguga@gmail.com>
2015-06-17 11:00:37 -0300

    IAPManager

---
+++ Assets/PowerUpLevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpLevelManager : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void Resume(GameObject obj)
+	{
+		Time.timeScale = 1;
+		obj.SetActive (false);
+	}
+}
---
+++ Assets/PowerUpManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpManager : MonoBehaviour 
+{
+	public string nome;
+	public GameObject shopPowerUp;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Utiliza()
+	{
+		Time.timeScale = 0;
+		if(PlayerPrefs.GetInt(nome) == 1)
+		{
+			//usa power up
+		}
+		else
+		{
+			shopPowerUp.SetActive(true);
+		}
+	}
+}
