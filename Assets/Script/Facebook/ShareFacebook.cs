69042a032fa90388ecb199ff045d09b9043367fe
Gabriel <mutatisguga@gmail.com>
2015-06-23 11:42:43 -0300

    d

--- Assets/Script/Facebook/ShareFacebook.cs
+++ Assets/Script/Facebook/ShareFacebook.cs
@@ -17,7 +17,7 @@ public class ShareFacebook : MonoBehaviour {
 
 	public void Share()
 	{
-		if(FB.IsLoggedIn true)
+		if(FB.IsLoggedIn)
 		{
 			FB.Feed(                                                                                                                 
 			        linkCaption: linkCaption,               
