6b56fbcd4de17bd0075948ee8501506023a6b9d5
Gabriel <mutatisguga@gmail.com>
2015-07-06 12:51:22 -0300

    Progress

---
+++ Assets/ProgressBar.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+
+	public Slider sli;
+
+	SceneMaster sceneMaster;
+
+	float maxValue;
+	float value;
+
+	// Use this for initialization
+	void Start ()
+	{
+		sceneMaster = GameObject.FindGameObjectWithTag ("SceneMaster").GetComponent<SceneMaster> ();
+		maxValue = sceneMaster.meta3Estrelas;
+		sli.maxValue = maxValue;
+		sli.value = maxValue;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		sli.value = maxValue - sceneMaster.Score;
+	}
+}
