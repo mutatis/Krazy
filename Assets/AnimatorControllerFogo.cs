96799e20131a6afdf889d723259874862ae005e4
Gabriel <mutatisguga@gmail.com>
2015-07-09 15:20:11 -0300

    Menu

---
+++ Assets/AnimatorControllerFogo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorControllerFogo : MonoBehaviour 
+{
+	public Animator anim;
+
+	public void Pode()
+	{
+		anim.SetTrigger("Clico");
+	}
+}
--- Assets/Script/Interface/AtivaDesativa.cs
+++ Assets/Script/Interface/AtivaDesativa.cs
@@ -3,15 +3,48 @@ using System.Collections;
 
 public class AtivaDesativa : MonoBehaviour
 {	
-	public void ControllAnim(Animator anim)
+	public Animator[] anim;
+	public Animator[] anim3;
+
+	public void ControllAnim(Animator anim2)
 	{
 		if(Time.timeScale == 1)
 		{
-			anim.SetTrigger("Desclico");
+			anim2.SetTrigger("Desclico");
+			for(int i = 0; i < anim.Length; i++)
+			{
+				anim[i].SetTrigger("Desclico");
+			}
+			for(int i = 0; i < anim3.Length; i++)
+			{
+				anim3[i].SetTrigger("Desclico");
+			}
 		}
 		else
 		{
-			anim.SetTrigger("Clico");
+			anim2.SetTrigger("Clico");
+		}
+	}
+
+	public void Va()
+	{
+		for(int i = 0; i < anim.Length; i++)
+		{
+			anim[i].SetTrigger("Clico");
+		}
+	}
+
+	public void Liga(GameObject obj)
+	{
+		if(Time.timeScale == 0)
+		{
+			//	obj.SetActive(false);
+			Time.timeScale = 1;
+		}
+		else
+		{
+			//obj.SetActive(true);
+			Time.timeScale = 0;
 		}
 	}
 
