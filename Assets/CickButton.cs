233882fdb96f57ada31ba07458d2f8974fb3de0b
Gabriel <mutatisguga@gmail.com>
2015-07-09 11:54:06 -0300

    Audios

---
+++ Assets/CickButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CickButton : MonoBehaviour
+{
+
+	public void Click(AudioClip audio)
+	{
+		AudioSource.PlayClipAtPoint (audio, transform.position);
+	}
+}
