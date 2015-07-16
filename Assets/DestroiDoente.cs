c3c4fc2e90dc6b2d7c949257841133f5f215a7ec
Gabriel <mutatisguga@gmail.com>
2015-07-16 12:39:29 -0300

    a

---
+++ Assets/DestroiDoente.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroiDoente : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine("GO");
+	}
+
+	IEnumerator GO()
+	{
+		yield return new WaitForSeconds (1);
+		Destroy (gameObject);
+	}
+}
