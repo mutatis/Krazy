db851af4c583e2b96e709385aa6e7eaf2e3bd1fc
Gabriel <mutatisguga@gmail.com>
2015-07-17 15:15:13 -0300

    Score

---
+++ Assets/Script/teste.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teste : MonoBehaviour 
+{
+	public Transform pos;
+
+	void Start()
+	{
+		transform.position = new Vector3 (pos.position.x, transform.position.y, transform.position.z);
+	}
+}
