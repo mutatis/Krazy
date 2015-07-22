276fd85a54a0b582f8e0e01d246083408c414d12
Westerbly Snaydley <westerbly@snaydley.com>
2015-07-22 15:03:23 -0300

    mostrar estrelas no mapa

--- Assets/Script/Manager/VerificaEstrelas.cs
+++ Assets/Script/Manager/VerificaEstrelas.cs
@@ -3,24 +3,19 @@ using System.Collections;
 
 public class VerificaEstrelas : MonoBehaviour 
 {
-	public string nomeFase;
+	public int idFase;
 	public GameObject[] estrelas;
-	int temp;
+	private GameMaster gameMaster;
 
 	// Use this for initialization
 	void Start () 
 	{
-		temp = PlayerPrefs.GetInt (nomeFase);
+		gameMaster = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameMaster>();
+		int qtdStars = gameMaster.GetStarsForLevel(idFase);
 
-		for(int i = 0; i < temp; i++)
+		for(int i = 0; i < qtdStars; i++)
 		{
 			estrelas[i].SetActive(true);
 		}
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-	
 	}
 }
