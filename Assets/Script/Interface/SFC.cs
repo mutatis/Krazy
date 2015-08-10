bdf03181d66887606d8e921edc891b19179cd8d2
Gabriel <mutatisguga@gmail.com>
2015-08-10 14:35:48 -0300

    icone CVS

--- Assets/Script/Interface/SFC.cs
+++ Assets/Script/Interface/SFC.cs
@@ -3,7 +3,25 @@ using System.Collections;
 
 public class SFC : MonoBehaviour 
 {
-	public void Link ()
+
+    public GameObject[] obj;
+
+    void Start()
+    {
+        obj[Random.Range(0, obj.Length)].SetActive(true);
+    }
+
+    public void CVS()
+    {
+#if UNITY_ANDROID
+        Application.OpenURL("https://play.google.com/store/apps/details?id=com.DagonStudios.Cthulhu");
+#endif
+#if UNITY_IOS
+		Application.OpenURL("https://itunes.apple.com/us/app/cthulhu-v-santa-claus-war/id957398997?l=pt&ls=1&mt=8");
+#endif
+    }
+
+    public void SFCG ()
 	{
 #if UNITY_ANDROID
 		Application.OpenURL("https://play.google.com/store/apps/details?id=com.DagonStudios.SuperFlappyCthulhuFinal");
